6cedae6407b1619aef4694faee3338a025c114ca
mgabys1 <mgabriela.83@gmail.com>
2024-06-23 20:46:11 -0400

    Update create-edit-task.page.ts

--- src/app/create-edit-task/create-edit-task.page.ts
+++ src/app/create-edit-task/create-edit-task.page.ts
@@ -13,13 +13,9 @@ export class CreateEditTaskPage {
 
   saveTask() {
     try {
-      // Lee las tareas del localStorage, si no existen inicializa como un array vacío
       const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-      // Añade la nueva tarea
       tasks.push({ task_name: this.taskName });
-      // Guarda las tareas actualizadas en localStorage
       localStorage.setItem('tasks', JSON.stringify(tasks));
-      // Navega de vuelta a la página de inicio
       this.router.navigate(['/home']);
     } catch (error) {
       console.error('Error saving task to localStorage:', error);
